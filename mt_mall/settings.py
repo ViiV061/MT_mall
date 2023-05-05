10e6bfafdc97989220ddbf96e5a2ca0a4ca9dd88
ViiV061 <bzwmz@msn.com>
2023-05-04 17:30:35 -0700

    set mysql & redis

--- mt_mall/settings.py
+++ mt_mall/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'mt_mall.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'USER': 'dev',  # user for development use
+        'PASSWORD': '12345',
+        'NAME': 'db_mt_mall',
     }
 }
 
@@ -122,3 +126,27 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    },
+    "session": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "session"
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
