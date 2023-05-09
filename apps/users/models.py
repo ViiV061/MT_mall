7f28bb005b47a97ddaa1a4b541542696b4af32ff
ViiV061 <bzwmz@msn.com>
2023-05-08 17:28:22 -0700

    users

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+# class User(models.Model):
+#     username = models.CharField(max_length=20, unique=True)
+#     password = models.CharField(max_length=20)
+#     email = models.EmailField(max_length=50, unique=True)
+
+
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    mobile = models.CharField(max_length=10, unique=True)
+
+    class Meta:
+        db_table = 'tb_users'
+        verbose_name = 'Users'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.username
